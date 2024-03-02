08509a77713a13760e8a25f7f8dd22b5e74017d1
Gilad Hanania גלעד חנניה <133251295+Dirkel69@users.noreply.github.com>
2024-03-02 17:59:35 +0200

    Source file of the class

---
+++ ShitPile.cs
@@ -0,0 +1,35 @@
+﻿namespace Test;
+
+class ShitPile<T>
+{
+    private T[] _arr = Array.Empty<T>();
+    public int Weigh => _arr.Length;
+    public void Shit(T item)
+    {
+        T[] newArr = new T[_arr.Length + 1];
+        for (int i = 0; i < _arr.Length; i++)
+            newArr[i] = _arr[i];
+        newArr[_arr.Length] = item; // Corrected line
+        _arr = newArr;
+    }
+    public T Sniff() => _arr[new Random().Next(0, _arr.Length)];
+    private void _Remove(int index)
+    {
+        if (index < 0 || index >= _arr.Length) throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range of the array.");
+
+        T[] newArr = new T[_arr.Length - 1];
+        for (int i = 0; i < index; i++)
+            newArr[i] = _arr[i];
+        for (int i = index + 1; i < _arr.Length; i++)
+            newArr[i - 1] = _arr[i];
+        _arr = newArr;
+    }
+    public T Wipe()
+    {
+        int index = new Random().Next(0, _arr.Length);
+        T result = _arr[index];
+        _Remove(index);
+        return result;
+    }
+    public void Flush() => _arr = Array.Empty<T>();
+}
